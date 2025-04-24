59f0470fd5b95f2b9536ba55084845881078bfe3
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2025-04-24 16:34:41 +0200

    initial

--- Assignment-2/dfs.pl
+++ Assignment-2/dfs.pl
@@ -1,1 +1,34 @@
 consult('data_process.pl').
+
+% validating the new position
+valid_move((X,Y),Visited) :-
+    \+ member((X,Y), Visited),
+    X >= 0,
+    Y >= 0,
+    grid_size(R,C),
+    X < R,
+    Y < C,
+    \+ obstacle(X,Y).
+
+left((X,Y),(X,NewY)) :-
+    NewY is Y - 1.
+
+right((X,Y),(X,NewY)) :-
+    NewY is Y + 1.
+
+up((X,Y),(NewX,Y)) :-
+    NewX is X - 1.
+
+down((X,Y),(NewX,Y)) :-
+    NewX is X + 1.
+
+move((X,Y), (NewX,NewY), Visited) :-
+    (left((X,Y), (NewX,NewY));
+     right((X,Y), (NewX,NewY));
+     up((X,Y), (NewX,NewY));
+     down((X,Y), (NewX,NewY))),
+    valid_move((NewX,NewY), Visited).
+
+% base case
+dfs((X,Y),Visited) :-
+    \+ move((X,Y), (NewX,NewY), Visited).
